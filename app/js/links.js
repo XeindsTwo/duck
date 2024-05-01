830cdf6d3b2e8aef3eddf0554c843b71d0ea4b23
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2024-05-01 15:28:47 +0300

    Версточка

---
+++ app/js/links.js
@@ -0,0 +1,32 @@
+export function links() {
+  const headerLinks = document.querySelectorAll('.header__link');
+  const sections = document.querySelectorAll('section');
+  const footer = document.querySelector('footer');
+
+  const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.3
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        const targetId = entry.target.getAttribute('id');
+        headerLinks.forEach(link => {
+          if (link.getAttribute('href') === `#${targetId}`) {
+            link.classList.add('active');
+          } else {
+            link.classList.remove('active');
+          }
+        });
+      }
+    });
+  }, observerOptions);
+
+  sections.forEach(section => {
+    observer.observe(section);
+  });
+
+  observer.observe(footer);
+}
--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,14 @@
+import {links} from "./links.js";
+import {ecosystem} from "./ecosystem.js";
+
+links();
+ecosystem();
+
 const button = document.querySelector('.home__subname');
 const contractParagraph = document.querySelector('.home__item p');
 const tooltip = document.querySelector('.home__tooltip');
 
-button.addEventListener('click', function() {
+button.addEventListener('click', function () {
   const contractAddress = contractParagraph.textContent;
 
   navigator.clipboard.writeText(contractAddress)
@@ -16,116 +22,4 @@ button.addEventListener('click', function() {
     .catch((error) => {
       console.error('Error when copying contract address:', error);
     });
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleItems = document.querySelectorAll(".ecosystem__content-item");
-  const percentageSpans = document.querySelectorAll(".ecosystem__left-line span");
-  const svgPath = document.querySelector(".ecosystem__left-line svg path");
-  const categoryNames = document.querySelectorAll(".ecosystem__item-name");
-  const bgItems = document.querySelectorAll(".ecosystem__item-bg");
-  const percentDigits = document.querySelectorAll(".ecosystem__digital");
-
-  for (let i = 1; i < percentageSpans.length; i++) {
-    percentageSpans[i].style.opacity = "0";
-    categoryNames[i].style.opacity = "0";
-    percentDigits[i].style.opacity = "0";
-    percentDigits[i].style.transform = "translateY(-45px)";
-  }
-
-  toggleItems.forEach(function (item, index) {
-    item.addEventListener("click", function () {
-      toggleItems.forEach(function (el) {
-        el.classList.remove("active");
-        const link = el.querySelector(".ecosystem__link");
-        link.style.opacity = "0";
-        link.style.height = "0";
-        link.style.marginTop = "0";
-      });
-
-      item.classList.add("active");
-      const activeCheckButton = item.querySelector(".ecosystem__link");
-      activeCheckButton.style.opacity = "1";
-      activeCheckButton.style.height = "auto";
-      activeCheckButton.style.marginTop = "24px";
-
-      const categoryItem = document.querySelector(".ecosystem__item");
-      categoryItem.classList = "ecosystem__item";
-      if (index === 0) {
-        categoryItem.classList.add("item-1");
-      } else {
-        categoryItem.classList.add(`item-${index + 1}`);
-      }
-
-      bgItems.forEach((bgItem, i) => {
-        if (i === index) {
-          bgItem.classList.add("active");
-        } else {
-          bgItem.classList.remove("active");
-        }
-      });
-
-      percentageSpans.forEach(function (span, i) {
-        span.style.transition = "opacity 0.7s ease-in-out";
-        span.style.opacity = "0";
-        if (i === index) {
-          setTimeout(function () {
-            span.style.opacity = "1";
-          }, 100);
-        }
-      });
-
-      categoryNames.forEach(function (name, i) {
-        name.style.transition = "opacity 0.8s ease-in-out";
-        name.style.opacity = "0";
-        if (i === index) {
-          setTimeout(function () {
-            name.style.opacity = "1";
-          }, 100);
-        }
-      });
-
-      percentDigits.forEach(function (digit, i) {
-        digit.style.transition = "opacity 0.9s ease-in-out, transform 0.9s ease-in-out";
-        digit.style.opacity = "0";
-        if (i === index) {
-          setTimeout(function () {
-            digit.style.opacity = "1";
-            digit.style.transform = "translateY(0)";
-          }, 100);
-        } else {
-          digit.style.opacity = "0";
-          digit.style.transform = "translateY(45px)";
-        }
-      });
-
-      setTimeout(function () {
-        percentDigits.forEach(function (digit, i) {
-          if (i !== index) {
-            digit.style.transform = "translateY(-45px)";
-          }
-        });
-      }, 800);
-
-      const colors = ['#fba01a', '#0288BB', '#AB50EA', '#3215D1', '#079837'];
-      svgPath.style.stroke = colors[index];
-      percentageSpans.forEach(function (span, i) {
-        span.style.color = colors[i];
-      });
-    });
-
-    if (index === 0) {
-      item.classList.add("active");
-      const firstLink = item.querySelector(".ecosystem__link");
-      firstLink.style.opacity = "1";
-      firstLink.style.height = "auto";
-      firstLink.style.marginTop = "24px";
-
-      const categoryItem = document.querySelector(".ecosystem__item");
-      categoryItem.classList = "ecosystem__item item-1";
-      bgItems[0].classList.add("active");
-      percentDigits[0].style.opacity = "1";
-      percentDigits[0].style.transform = "translateY(0)";
-    }
-  });
 });
