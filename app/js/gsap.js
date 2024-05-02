2b1750557fe1e4b39db5005534bde1694b8dc98d
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2024-05-02 23:21:08 +0300

    adaptive home, gsap

--- app/js/gsap.js
+++ app/js/gsap.js
@@ -1,6 +1,20 @@
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 const timeline = gsap.timeline();
+
+const homeSection = document.querySelector('.home');
+const duckImage = document.querySelector('.duck');
+
+gsap.set(duckImage, {opacity: 0});
+
+ScrollTrigger.create({
+  trigger: homeSection,
+  start: 'top 80%',
+  onEnter: () => {
+    gsap.to(duckImage, {duration: 0.7, opacity: 1, delay: 1});
+  }
+});
+
 const roadmapItems = document.querySelectorAll('.roadmap__item');
 
 const animateItem = (item, duration) => {
@@ -44,6 +58,25 @@ ScrollTrigger.create({
   }
 });
 
+const exploreSection = document.querySelector('.explore');
+const exploreImg = document.querySelector('.explore__img');
+const initialY = window.innerWidth < 1100 ? 800 : 1100;
+gsap.set(exploreImg, {y: initialY});
+
+ScrollTrigger.create({
+  trigger: exploreSection,
+  start: 'top 80%',
+  onEnter: () => {
+    gsap.to(exploreImg, {
+      duration: 11.7,
+      opacity: 1,
+      y: -810,
+      delay: 0.2,
+      ease: "power1.inOut",
+    });
+  }
+});
+
 const sections = document.querySelectorAll(".section");
 
 const scrolling = {
@@ -51,14 +84,14 @@ const scrolling = {
   events: "scroll,wheel,touchmove,pointermove".split(","),
   prevent: e => e.preventDefault(),
   disable() {
-    if (scrolling.enabled) {
+    if (scrolling.enabled && window.innerWidth >= 1100) {
       scrolling.enabled = false;
       window.addEventListener("scroll", gsap.ticker.tick, {passive: true});
       scrolling.events.forEach((e, i) => (i ? document : window).addEventListener(e, scrolling.prevent, {passive: false}));
     }
   },
   enable() {
-    if (!scrolling.enabled) {
+    if (!scrolling.enabled && window.innerWidth >= 1100) {
       scrolling.enabled = true;
       window.removeEventListener("scroll", gsap.ticker.tick);
       scrolling.events.forEach((e, i) => (i ? document : window).removeEventListener(e, scrolling.prevent));
@@ -67,7 +100,7 @@ const scrolling = {
 };
 
 function goToSection(section) {
-  if (scrolling.enabled) {
+  if (scrolling.enabled && window.innerWidth >= 1100) {
     scrolling.disable();
     gsap.to(window, {
       scrollTo: {y: section, autoKill: false},
